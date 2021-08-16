08e9a3e5b558459e83d237fbc354380b1bdca405
Kamal Mango <kamal.mango7@gmail.com>
2021-08-16 20:01:46 +0200

    add pie chart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,26 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Yellow', 'Green'],
+    datasets: [{
+      label: 'sentiment',
+      data: [0, 0, 0],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)'
+      ],
+      borderWidth: 1
+    }]
+  }
+})
+
 const submitReview = (e) => {
   e.preventDefault();
   const review = document.getElementById('review').value;
@@ -10,6 +33,7 @@ const submitReview = (e) => {
   const emojiSection = document.getElementById('emojiSection');
   const title = document.getElementById('title');
   const outline = document.querySelector(':focus');
+
 
   fetch('/api/nlp/s-analyzer', options)
     .then(res => res.json())
@@ -30,6 +54,13 @@ const submitReview = (e) => {
         title.style.color = 'green';
         outline.style.borderColor = 'green'
       }
+
+      chart.data.datasets.forEach(dataset => {
+        dataset.data = [intensity.neg, intensity.neu, intensity.pos]
+        console.log(dataset.data)
+      })
+      chart.update()
+
     })
     .catch(err => {
       emojiSection.innerHTML = 'There was an error processing your request!'
